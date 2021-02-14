0d03f55dcf9bda40eef228c5b7fafb24de8a0e48
sharang1996 <sharang.gupta@sitpune.edu.in>
2021-02-14 03:13:15 +0000

    bankist started!

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -24,6 +24,7 @@ createBooking(123, 2);
 createBooking(123, undefined, 500);
 
 // How Passing Arguments Works: Values vs. Reference
+/*
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -55,6 +56,7 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
 
 // Functions Accepting Callback Functions
 const oneWord = function (str) {
@@ -101,3 +103,286 @@ greet('Hello')('Jonas');
 const greetArr = greeting => name => console.log(`${greeting} ${name}`);
 
 greetArr('Hi')('Jonas');
+
+const lufthansa = {
+  airline: 'lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name })
+  }
+
+}
+
+lufthansa.book(239, 'Sharang');
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: 'EW',
+  bookings: []
+
+}
+
+const book = lufthansa.book;
+
+//gives an error as this points to undefined for a regular function
+//book(23, 'Sophie');
+
+//use call to invoke function with your own this parameter
+book.call(lufthansa, 239, 'Sophie');
+console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss",
+  iataCode: 'LX',
+  bookings: []
+
+}
+
+book.call(swiss, 69, 'Sharang');
+book.call(swiss, 69, 'Sophie');
+console.log(swiss);
+
+//apply
+const flightData = [127, 'George'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+//could do this instead of apply:
+book.call(eurowings, ...flightData);
+console.log(eurowings);
+
+//bind method
+const bookEw = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEw(127, 'emma watson');
+console.log(eurowings);
+
+bookLH(127, 'emma watson');
+console.log(lufthansa);
+
+bookLX(127, 'emma watson');
+console.log(swiss);
+
+//setting parameters beforehand is called partial application
+const bookEw23 = book.bind(eurowings, 23);
+bookEw23('Maisie Williams');
+console.log(eurowings);
+
+//with event listeners
+lufthansa.planes = 300;
+
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+
+  console.log(this.planes);
+}
+
+//in an event handler this keyword is always attached to the element pointed to
+document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//partial application
+const addTax = (rate, value) => rate * value + value;
+console.log(addTax(0.1, 100));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+function vatCalcFactory(rate) {
+  return function (value) {
+    return rate * value + value;
+  }
+}
+const calc = vatCalcFactory(0.55);
+console.log(calc(44000));
+
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const inp = Number(prompt(`What is your favourite programming language?
+    \n0: JavaScript
+    \n1: Python
+    \n2: Rust
+    \n3: C++
+    \n(Write option number)`));
+
+    if (typeof inp === "number" && inp < 4 && inp >= 0) {
+      this.answers[inp]++;
+    }
+    console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    }
+    else if (type == 'string') {
+      let str = '';
+      for (const i of this.answers) {
+        str = str + i + ', ';
+      }
+      console.log(`poll results are ${str}`);
+    }
+  },
+
+}
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+
+///////////////////////////////////////
+// Immediately Invoked Function Expressions (IIFE)
+const runOnce = function () {
+  console.log('This will never run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
+
+
+///////////////////////////////////////
+// Closures
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+
+
+///////////////////////////////////////
+// More Closure Examples
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+/*
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+*/
